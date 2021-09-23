b63bed4886c59e455c900154b57e580d813c54f2
CrazyClone55 <shaan@webitup.com>
2021-09-23 10:09:16 -0500

    Edited Main.java

--- Activity 4/Main.java
+++ Activity 4/Main.java
@@ -2,5 +2,6 @@ public class Main{
 	public static void main(String[] args)throws Exception{
 		System.out.println("Shaan");
 		System.out.println("Patel");
+		System.out.println("spatel23@sjs.org");
 	}
 }
